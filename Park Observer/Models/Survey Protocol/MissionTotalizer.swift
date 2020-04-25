674de55d1346e1af37069b69c7470742e3be4c94
Regan Sarwas <regan_sarwas@nps.gov>
2020-04-24 22:06:24 -0800

    Add Totalizer Tests

--- Park Observer/Models/Survey Protocol/MissionTotalizer.swift
+++ Park Observer/Models/Survey Protocol/MissionTotalizer.swift
@@ -13,26 +13,26 @@ struct MissionTotalizer: Codable {
   let fields: [String]
 
   /// The size (in points) of the font used for the totalizer text.
-  let fontsize: Double?
+  let fontSize: Double
 
   /// Indicate if the total distance/time while not 'observing' should be displayed.
-  let includeoff: Bool?
+  let includeOff: Bool
 
   /// Indicate if the total distance/time while 'observing' should be displayed.
-  let includeon: Bool?
+  let includeOn: Bool
 
   /// Indicate if the total distance/time regardless of 'observing' status should be displayed.
-  let includetotal: Bool?
+  let includeTotal: Bool
 
   /// The units for the quantities displayed in the totalizer.
-  let units: TotalizerUnits?
+  let units: TotalizerUnits
 
   enum CodingKeys: String, CodingKey {
     case fields = "fields"
-    case fontsize = "fontsize"
-    case includeoff = "includeoff"
-    case includeon = "includeon"
-    case includetotal = "includetotal"
+    case fontSize = "fontsize"
+    case includeOff = "includeoff"
+    case includeOn = "includeon"
+    case includeTotal = "includetotal"
     case units = "units"
   }
 
@@ -44,3 +44,46 @@ struct MissionTotalizer: Codable {
   }
 
 }
+
+//MARK: - MissionTotalizer Codable
+// Custom decoding to do array checking:
+// Fields must have at least one element,
+//   and all elements must be unique
+// Also a good time to set default values
+
+extension MissionTotalizer {
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let fields = try container.decode([String].self, forKey: .fields)
+    let fontSize = try container.decodeIfPresent(Double.self, forKey: .fontSize) ?? 14.0
+    let includeOff = try container.decodeIfPresent(Bool.self, forKey: .includeOff) ?? false
+    let includeOn = try container.decodeIfPresent(Bool.self, forKey: .includeOn) ?? true
+    let includeTotal = try container.decodeIfPresent(Bool.self, forKey: .includeTotal) ?? false
+    let units = try container.decodeIfPresent(TotalizerUnits.self, forKey: .units) ?? .kilometers
+    // Validate fields and fontSize
+    if fields.count == 0 {
+      throw ParsingError.arrayEmpty //TODO: Throw decoding error with context
+    }
+    if Set(fields).count != fields.count {
+      throw ParsingError.nonuniqueArray //TODO: Throw decoding error with context
+    }
+    if fontSize < 0 {
+      throw ParsingError.negativeNumber //TODO: Throw decoding error with context
+    }
+    self.init(
+      fields: fields,
+      fontSize: fontSize,
+      includeOff: includeOff,
+      includeOn: includeOn,
+      includeTotal: includeTotal,
+      units: units)
+  }
+
+  enum ParsingError: Error {
+    case arrayEmpty
+    case nonuniqueArray
+    case negativeNumber
+  }
+}
+
--- Park ObserverTests/Survey Protocol/TotalizerTests.swift
+++ Park ObserverTests/Survey Protocol/TotalizerTests.swift
@@ -8,9 +8,371 @@
 
 import XCTest
 
+@testable import Park_Observer
+
 class TotalizerTests: XCTestCase {
 
-  func testTotalizer() {
+  func testTotalizerEmpty() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {}
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNil(json)
+  }
+
+  func testTotalizerMinimal() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["one"]
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNotNil(json)
+    if let test = json {
+      XCTAssertEqual(test.totalizer.fields.count, 1)
+      XCTAssertEqual(test.totalizer.fields[0], "one")
+      XCTAssertTrue(test.totalizer.includeOn)
+      XCTAssertFalse(test.totalizer.includeOff)
+      XCTAssertFalse(test.totalizer.includeTotal)
+      XCTAssertEqual(test.totalizer.fontSize, 14.0, accuracy: 0.001)
+      XCTAssertEqual(test.totalizer.units, .kilometers)
+    }
+  }
+
+  func testTotalizerTwoFields() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["one", "two"]
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNotNil(json)
+    if let test = json {
+      XCTAssertEqual(test.totalizer.fields.count, 2)
+      XCTAssertEqual(test.totalizer.fields[0], "one")
+      XCTAssertEqual(test.totalizer.fields[1], "two")
+    }
+  }
+
+  func testTotalizerEmptyFields() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": []
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNil(json)
+  }
+
+  func testTotalizerNonuniqueFields() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["one", "one"]
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNil(json)
+  }
+
+  func testTotalizerUnits1() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["f1"],
+          "units": "kilometers"
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNotNil(json)
+    if let test = json {
+      XCTAssertEqual(test.totalizer.units, .kilometers)
+    }
+  }
+
+  func testTotalizerUnits2() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["f1"],
+          "units": "minutes"
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNotNil(json)
+    if let test = json {
+      XCTAssertEqual(test.totalizer.units, .minutes)
+    }
+  }
+
+  func testTotalizerUnits3() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["f1"],
+          "units": "miles"
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNotNil(json)
+    if let test = json {
+      XCTAssertEqual(test.totalizer.units, .miles)
+    }
+  }
+
+  func testTotalizerBadUnits() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["f1"],
+          "units": "dog-years"
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNil(json)
+  }
+
+  func testTotalizerInvalidUnits() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["f1"],
+          "units": 14
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNil(json)
+  }
+
+  func testTotalizerBooleansTrue() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["one"],
+          "includeon": true,
+          "includeoff": true,
+          "includetotal": true
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNotNil(json)
+    if let test = json {
+      XCTAssertTrue(test.totalizer.includeOn)
+      XCTAssertTrue(test.totalizer.includeOff)
+      XCTAssertTrue(test.totalizer.includeTotal)
+    }
+  }
+
+  func testTotalizerBooleansFalse() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["one"],
+          "includeon": false,
+          "includeoff": false,
+          "includetotal": false
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNotNil(json)
+    if let test = json {
+      XCTAssertFalse(test.totalizer.includeOn)
+      XCTAssertFalse(test.totalizer.includeOff)
+      XCTAssertFalse(test.totalizer.includeTotal)
+    }
+  }
+
+  func testTotalizerFontSize() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["one"],
+          "fontsize": 18.5
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNotNil(json)
+    if let test = json {
+      XCTAssertEqual(test.totalizer.fontSize, 18.5, accuracy: 0.001)
+    }
+  }
+
+  func testTotalizerFontSizeInvalid() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["one"],
+          "fontsize": "18.5"
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNil(json)
+  }
+
+  func testTotalizerFontSizeNegative() {
+    // Given:
+    struct TestJson: Codable {
+      let totalizer: MissionTotalizer
+    }
+    let jsonData = Data(
+      """
+      {
+        "totalizer": {
+          "fields": ["one"],
+          "fontsize": -11.5
+        }
+      }
+      """.utf8)
+
+    // When:
+    let json = try? JSONDecoder().decode(TestJson.self, from: jsonData)
+
+    // Then:
+    XCTAssertNil(json)
   }
 
 }
