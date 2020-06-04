3c6ad31d27b7a0e6ea94ceb6e41dc22e1fadfc6e
Regan Sarwas <regan_sarwas@nps.gov>
2020-06-04 14:23:04 -0800

    Add stubs for missing CoreData entity class tests

---
+++ Park ObserverTests/Model/CoreData/GpsPointTests.swift
@@ -0,0 +1,19 @@
+//
+//  GpsPointTests.swift
+//  Park ObserverTests
+//
+//  Created by Regan E. Sarwas on 6/4/20.
+//  Copyright © 2020 Alaska Region GIS Team. All rights reserved.
+//
+
+import XCTest
+
+@testable import Park_Observer
+
+class GpsPointTests: XCTestCase {
+
+  func testLocation() {
+    XCTAssertTrue(false)
+  }
+
+}
---
+++ Park ObserverTests/Model/CoreData/ObservationTests.swift
@@ -0,0 +1,27 @@
+//
+//  ObservationTests.swift
+//  Park ObserverTests
+//
+//  Created by Regan E. Sarwas on 6/4/20.
+//  Copyright © 2020 Alaska Region GIS Team. All rights reserved.
+//
+
+import XCTest
+
+@testable import Park_Observer
+
+class ObservationTests: XCTestCase {
+
+  func testTimestamp() {
+    XCTAssertTrue(false)
+  }
+
+  func testLocationOfFeature() {
+    XCTAssertTrue(false)
+  }
+
+  func testRequestLocationOfObserver() {
+    XCTAssertTrue(false)
+  }
+
+}
