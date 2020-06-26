c2de64fde061fe6c5e7aabc0c8877032686dd20b
Regan Sarwas <Regan_Sarwas@nps.gov>
2020-06-26 09:37:42 -0800

    Export survey if it is not currently tracklogging

--- Park Observer/Views/SlideOutViews/FileItemView.swift
+++ Park Observer/Views/SlideOutViews/FileItemView.swift
@@ -53,12 +53,13 @@ struct MapItemView: View {
 
 struct SurveyItemView: View {
   var name: String
+  @State private var isExporting = false
   @State private var infoMessage: String? = nil
   @State private var errorMessage: String? = nil
   @EnvironmentObject var surveyController: SurveyController
 
   var body: some View {
-    //TODO: 0) implement export survey
+    //TODO: 1) Support other conflict resolution strategies
     //TODO: 2) replace file name with title from info; add icon, dates and status
     //TODO: 3) navigate to additional info about the survey
     VStack(alignment: .leading) {
@@ -80,24 +81,28 @@ struct SurveyItemView: View {
         }
         Spacer()
         Button(action: {
-          do {
-            // start as progress indicator, and do export in background
-            throw ImportError.exists
-            // If the survey is loaded (name == SurveyController.surveyName) then
-            // we should stop any editing (tracklogging) and save
-            //  we could ask the surveyController to export the current survey
-            // otherwise, we need to load and then export the survey
-            // maybe add a static method on Survey to load and export the survey with name
-            // Survey.export(name, conflict: .fail)
-            //_ = try FileManager.default.importSurvey(from: self.name, conflict: .fail)
-            //self.errorMessage = "Success!"
-          } catch {
-            self.errorMessage = "Unable to export survey"
+          self.infoMessage = nil
+          self.errorMessage = nil
+          self.isExporting = true
+          Survey.export(self.name, conflict: .fail) { error in
+            if let error = error {
+              self.errorMessage = error.localizedDescription
+            } else {
+              self.infoMessage = "Exported survey"
+            }
+            self.isExporting = false
           }
         }) {
-          Image(systemName: "tray.and.arrow.up")
+          if self.isExporting {
+            // In Xcode 12 (beta) use ProgressView()
+            // Until then, wrap UIActivityIndicatorView (https://stackoverflow.com/a/56496896)
+            Image(systemName: "staroflife.fill")
+          } else {
+            Image(systemName: "tray.and.arrow.up")
+          }
         }
         .buttonStyle(BorderlessButtonStyle())
+        .disabled(self.isExporting || (surveyController.surveyName == name && surveyController.tracklogging))
       }
       if errorMessage != nil {
         Text(errorMessage!).font(.caption).foregroundColor(.red)
