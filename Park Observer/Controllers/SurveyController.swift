39c07d931becb4b80fd24116c14ceed7e008e43d
Regan Sarwas <Regan_Sarwas@nps.gov>
2020-06-29 21:09:49 -0800

    Reorganize SurveyController

--- Park Observer/Controllers/SurveyController.swift
+++ Park Observer/Controllers/SurveyController.swift
@@ -25,7 +25,7 @@ import ArcGIS  // For AGSMapView and AGSGeoViewTouchDelegate
 import CoreLocation  // For CLLocationManagerDelegate
 import Foundation  // For NSObject (for delegates)
 
-class SurveyController: NSObject, ObservableObject, CLLocationManagerDelegate,
+class SurveyController: NSObject, ObservableObject,
   AGSGeoViewTouchDelegate
 {
 
@@ -102,6 +102,8 @@ class SurveyController: NSObject, ObservableObject, CLLocationManagerDelegate,
   private var mission: Mission? = nil
   private var missionProperty: MissionProperty? = nil
 
+  //MARK: - Initialize
+
   override init() {
     locationButtonController = LocationButtonController(mapView: self.mapView)
     viewPointController = ViewPointController(mapView: self.mapView)
@@ -167,25 +169,13 @@ class SurveyController: NSObject, ObservableObject, CLLocationManagerDelegate,
     }
   }
 
-  func save(_ survey: Survey?) {
-    do {
-      try survey?.save()
-    } catch {
-      message = Message.error("Unable to save survey: \(error.localizedDescription)")
-    }
-  }
-
   //MARK: - Save/Restore State
 
   func saveState() {
     // To be called when the app goes into the background
     // If the app is terminated this state can be restored when the app relaunches.
-    print("SurveyController.saveState() called on main thread: \(Thread.isMainThread)")
-    do {
-      try survey?.save()
-    } catch {
-      print("Error saving survey: \(error)")
-    }
+    //print("SurveyController.saveState() called on main thread: \(Thread.isMainThread)")
+    save(survey)
     Defaults.mapName.write(mapName)
     Defaults.surveyName.write(surveyName)
     locationButtonController.saveState()
@@ -202,22 +192,12 @@ class SurveyController: NSObject, ObservableObject, CLLocationManagerDelegate,
     slideOutMenuWidth = slideOutMenuWidth < 10.0 ? 300.0 : slideOutMenuWidth
   }
 
-  //MARK: - Background Locations
-
-  private var savedLocations = [CLLocation]()
-
-  func startBackgroundLocations() {
-    locationManager.allowsBackgroundLocationUpdates =
-      trackLogging && self.enableBackgroundTrackLogging && gpsAuthorization == .background
-    //print("In background. Locations enabled: \(locationManager.allowsBackgroundLocationUpdates)")
-  }
-
-  func drawBackgroundLocations() {
-    //print("Drawing \(savedLocations.count) locations collected in the background")
-    for location in savedLocations {
-      addGpsLocation(location)
+  private func save(_ survey: Survey?) {
+    do {
+      try survey?.save()
+    } catch {
+      message = Message.error("Unable to save survey: \(error.localizedDescription)")
     }
-    savedLocations.removeAll()
   }
 
   //MARK: - Adding GPS Locations
@@ -228,6 +208,7 @@ class SurveyController: NSObject, ObservableObject, CLLocationManagerDelegate,
   private var awaitingLocationForMissionProperty = false
   private var awaitingLocationForFeatureAtIndex = -1
   private var previousGpsPoint: GpsPoint? = nil
+  private var savedLocations = [CLLocation]()
 
   func stopTrackLogging() {
     observing = false
@@ -274,7 +255,7 @@ class SurveyController: NSObject, ObservableObject, CLLocationManagerDelegate,
     // Cycle the Location Manager to get the current location
     locationManager.stopUpdatingLocation()
     locationManager.startUpdatingLocation()
-    // mission property will be added when we the next GPS location
+    // mission property will be added when we get the next GPS location
   }
 
   func addObservationAtGps(featureIndex: Int) {
@@ -291,7 +272,7 @@ class SurveyController: NSObject, ObservableObject, CLLocationManagerDelegate,
     // Cycle the Location Manager to get the current location
     locationManager.stopUpdatingLocation()
     locationManager.startUpdatingLocation()
-    // feature will be added when we the next GPS location
+    // feature will be added when we get the next GPS location
   }
 
   /// Called by the Core Location Delegate whenever a new (or updated) location is available
@@ -373,202 +354,22 @@ class SurveyController: NSObject, ObservableObject, CLLocationManagerDelegate,
     }
   }
 
-}
+  //MARK: - Background Locations
 
-//MARK: - Survey Drawing
+  func startBackgroundLocations() {
+    locationManager.allowsBackgroundLocationUpdates =
+      trackLogging && self.enableBackgroundTrackLogging && gpsAuthorization == .background
+    //print("In background. Locations enabled: \(locationManager.allowsBackgroundLocationUpdates)")
+  }
 
-//TODO: Move to a separate file
-
-extension GpsPoint {
-
-  var asGraphic: AGSGraphic? {
-    guard let location = self.location else {
-      return nil
+  func drawBackgroundLocations() {
+    //print("Drawing \(savedLocations.count) locations collected in the background")
+    for location in savedLocations {
+      addGpsLocation(location)
     }
-    let agsPoint = AGSPoint(clLocationCoordinate2D: location)
-    //TODO: add attributes?
-    return AGSGraphic(geometry: agsPoint, symbol: nil, attributes: nil)
-  }
-}
-
-extension MissionProperty {
-
-  var asGraphic: AGSGraphic? {
-    guard let location = self.gpsPoint?.location ?? self.adhocLocation?.location else {
-      return nil
-    }
-    let agsPoint = AGSPoint(clLocationCoordinate2D: location)
-    //TODO: add attributes?
-    return AGSGraphic(geometry: agsPoint, symbol: nil, attributes: nil)
-  }
-}
-
-extension Observation {
-
-  func asGraphic(for feature: Feature) -> AGSGraphic? {
-    guard let location = self.locationOfFeature else {
-      return nil
-    }
-    let agsPoint = AGSPoint(clLocationCoordinate2D: location)
-    let attributes = self.attributes(for: feature)
-    return AGSGraphic(geometry: agsPoint, symbol: nil, attributes: attributes)
-  }
-}
-
-extension Survey {
-
-  var gpsGraphics: [AGSGraphic] {
-    guard let gpsPoints = try? self.viewContext.fetch(GpsPoints.allOrderByTime) else {
-      return []
-    }
-    return gpsPoints.compactMap { $0.asGraphic }
+    savedLocations.removeAll()
   }
 
-  var missionPropertyGraphics: [AGSGraphic] {
-    guard let missionProperties = try? self.viewContext.fetch(MissionProperties.fetchRequest) else {
-      return []
-    }
-    return missionProperties.compactMap { $0.asGraphic }
-  }
-
-  func featureGraphics(for feature: Feature) -> [AGSGraphic] {
-    guard let features = try? self.viewContext.fetch(Observations.fetchAll(for: feature.name))
-    else {
-      return []
-    }
-    return features.compactMap { $0.asGraphic(for: feature) }
-  }
-}
-
-extension AGSMapView {
-
-  func draw(_ survey: Survey) {
-    self.removeLayers()
-    self.addLayers(for: survey)
-    self.addGpsPoints(from: survey)
-    self.addTrackLogs(from: survey)
-    self.addMissionProperties(from: survey)
-    self.addFeatures(from: survey)
-  }
-
-  func removeLayers() {
-    self.graphicsOverlays.removeAllObjects()
-  }
-
-  //IMPORTANT - Keep layer indices consistent with the I create them
-  // the mapView owns the layers, but I need to access them by content/function
-  // There is a overlayID is can set on each layer, but that would require searching
-  // the layer list everytime I added a graphic.  Because I control the order that
-  // layers are added to the map, I can hard code the layer index for fast access.
-
-  func addLayers(for survey: Survey) {
-    let missionRenderers: [AGSRenderer?] = [
-      survey.config.mission?.gpsSymbology,
-      // TODO: Use one layer with a Unique Value Renderer
-      survey.config.mission?.onSymbology,
-      survey.config.mission?.offSymbology,
-      survey.config.mission?.symbology,
-    ]
-    let featureRenderers: [AGSRenderer?] = survey.config.features.map { $0.symbology }
-    for renderer in missionRenderers + featureRenderers {
-      let overlay = AGSGraphicsOverlay()
-      overlay.renderer = renderer
-      self.graphicsOverlays.add(overlay)
-    }
-  }
-
-  var gpsOverlay: AGSGraphicsOverlay {
-    self.graphicsOverlays[0] as! AGSGraphicsOverlay
-  }
-
-  var observingOverlay: AGSGraphicsOverlay {
-    self.graphicsOverlays[1] as! AGSGraphicsOverlay
-  }
-
-  var notObservingOverlay: AGSGraphicsOverlay {
-    self.graphicsOverlays[2] as! AGSGraphicsOverlay
-  }
-
-  var missionPropertyOverlay: AGSGraphicsOverlay {
-    self.graphicsOverlays[3] as! AGSGraphicsOverlay
-  }
-
-  func featureOverlay(at index: Int) -> AGSGraphicsOverlay {
-    self.graphicsOverlays[4 + index] as! AGSGraphicsOverlay
-  }
-
-  func addGpsPoint(_ gpsPoint: GpsPoint) {
-    if let graphic = gpsPoint.asGraphic {
-      let overlay = self.gpsOverlay
-      overlay.graphics.add(graphic)
-    }
-  }
-
-  func addGpsPoints(from survey: Survey) {
-    let overlay = self.gpsOverlay
-    overlay.graphics.addObjects(from: survey.gpsGraphics)
-  }
-
-  func addMissionProperty(_ missionProperty: MissionProperty) {
-    if let graphic = missionProperty.asGraphic {
-      let overlay = self.missionPropertyOverlay
-      overlay.graphics.add(graphic)
-    }
-  }
-
-  func addMissionProperties(from survey: Survey) {
-    let overlay = self.missionPropertyOverlay
-    overlay.graphics.addObjects(from: survey.missionPropertyGraphics)
-  }
-
-  func addTrackLogSegment(from point1: GpsPoint, to point2: GpsPoint, observing: Bool) {
-    guard let location1 = point1.location, let location2 = point2.location else {
-      return
-    }
-    let agsPoint1 = AGSPoint(clLocationCoordinate2D: location1)
-    let agsPoint2 = AGSPoint(clLocationCoordinate2D: location2)
-    let polyline = AGSPolyline(points: [agsPoint1, agsPoint2])
-    //TODO: add attributes?
-    let graphic = AGSGraphic(geometry: polyline, symbol: nil, attributes: nil)
-    let overlay = observing ? self.observingOverlay : self.notObservingOverlay
-    overlay.graphics.add(graphic)
-  }
-
-  func addTrackLogs(from survey: Survey) {
-    let overlayOn = self.observingOverlay
-    let overlayOff = self.notObservingOverlay
-    if let trackLogs = try? TrackLogs.fetchAll(context: survey.viewContext) {
-      for trackLog in trackLogs {
-        if let polyline = trackLog.polyline {
-          let graphic = AGSGraphic(geometry: polyline, symbol: nil, attributes: nil)
-          //TODO: add attributes?
-          if let observing = trackLog.properties.observing, observing {
-            overlayOn.graphics.add(graphic)
-          } else {
-            overlayOff.graphics.add(graphic)
-          }
-        }
-      }
-    }
-  }
-
-  func addFeature(_ observation: Observation, feature: Feature, index: Int) {
-    if let graphic = observation.asGraphic(for: feature) {
-      let overlay = self.featureOverlay(at: index)
-      overlay.graphics.add(graphic)
-    }
-  }
-
-  func addFeatures(from survey: Survey) {
-    for (index, feature) in survey.config.features.enumerated() {
-      let overlay = self.featureOverlay(at: index)
-      if let labelDef = feature.label?.labelDefinition() {
-        overlay.labelDefinitions.add(labelDef)
-        overlay.labelsEnabled = true
-      }
-      overlay.graphics.addObjects(from: survey.featureGraphics(for: feature))
-    }
-  }
 }
 
 //TODO: Move to a separate file
@@ -600,7 +401,7 @@ extension SurveyController {
 
 //MARK: - CoreLocation Manager Delegate
 
-extension SurveyController {
+extension SurveyController: CLLocationManagerDelegate {
 
   func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
     message = Message.warning(error.localizedDescription)
