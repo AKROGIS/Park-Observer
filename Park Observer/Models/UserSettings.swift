d70bac083d401d997de9f1c70b2e1ed9d626030b
Regan Sarwas <Regan_Sarwas@nps.gov>
2020-08-05 10:00:13 -0800

    documment UserSettings and add surveyControlsOnBottom

--- Park Observer/Models/UserSettings.swift
+++ Park Observer/Models/UserSettings.swift
@@ -10,13 +10,14 @@ import SwiftUI
 
 class UserSettings: ObservableObject {
 
-  /// MapControls can be light (for dark colored maps), or dark (for light maps)
+  /// Map Controls can be light (for dark colored maps), or dark (for light maps)
   @Published var darkMapControls = false
-
-  @Published var mapControlsSize: MapControlSize = .small
 
   /// Minimum required accuracy (in meters) of the GPS points
   @Published var gpsAccuracy = 0.0
+
+  /// Map Controls can be made larger for bumpy scenarios
+  @Published var mapControlsSize: MapControlSize = .small
 
   /// An Alarm clock control for countdown alerts
   @Published var showAlarmClock = false
@@ -27,34 +28,41 @@ class UserSettings: ObservableObject {
   /// Display of how long the user has been surveying (on transect/observing)
   @Published var showTotalizer = false
 
+  /// The survey control buttons can be on the top or bottom of the screen
+  @Published var surveyControlsOnBottom = false
+
   /// Units to display for the length of the survey
   @Published var totalizerUnits = MissionTotalizer.TotalizerUnits.kilometers
-
-  /// When streaming points for the tracklog, wait x seconds between successive points
-  @Published var tracklogIntervalTime = 0.0
 
   /// When streaming points for the tracklog, wait x meters between successive points
   @Published var tracklogIntervalDistance = 0.0
 
+  /// When streaming points for the tracklog, wait x seconds between successive points
+  @Published var tracklogIntervalTime = 0.0
+
   /// When streaming points for the tracklog, use time or distance
   @Published var tracklogIntervalUnits = TracklogIntervalUnits.off
 
+  /// Read the user defaults from the persisted defaults database
   func restoreState() {
     darkMapControls = Defaults.darkMapControls.readBool()
-    mapControlsSize = Defaults.mapControlsSize.readMapControlSize()
     gpsAccuracy = Defaults.gpsAccuracy.readDouble()
+    mapControlsSize = Defaults.mapControlsSize.readMapControlSize()
     showAlarmClock = Defaults.showAlarmClock.readBool()
     showInfoBanner = Defaults.showInfoBanner.readBool()
     showTotalizer = Defaults.showTotalizer.readBool()
+    surveyControlsOnBottom = Defaults.surveyControlsOnBottom.readBool()
   }
 
+  /// Save the user defaults to the persisted defaults database
   func saveState() {
     Defaults.darkMapControls.write(darkMapControls)
-    Defaults.mapControlsSize.write(mapControlsSize)
     Defaults.gpsAccuracy.write(gpsAccuracy)
+    Defaults.mapControlsSize.write(mapControlsSize)
     Defaults.showAlarmClock.write(showAlarmClock)
     Defaults.showInfoBanner.write(showInfoBanner)
     Defaults.showTotalizer.write(showTotalizer)
+    Defaults.surveyControlsOnBottom.write(surveyControlsOnBottom)
   }
 
 }
