a311f5e87ddc52039336b5e8baff0191ec196f5f
Regan Sarwas <rsarwas@gmail.com>
2021-10-16 15:40:19 -0800

    Replace deprecated (ios14) test metric

--- Park ObserverUITests/Park_ObserverUITests.swift
+++ Park ObserverUITests/Park_ObserverUITests.swift
@@ -38,7 +38,7 @@ class Park_ObserverUITests: XCTestCase {
   func testLaunchPerformance() {
     if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
       // This measures how long it takes to launch your application.
-      measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+      measure(metrics: [XCTApplicationLaunchMetric()]) {
         XCUIApplication().launch()
       }
     }
